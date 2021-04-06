c28850e9bfee717223c915f5682e048bc6d65fc3
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-04-05 21:49:25 -0700

    add initial files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// server listener
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
